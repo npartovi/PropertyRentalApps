53c5dfc060c4ea2a0c15f732f8ffe3f447933465
npartovi <npartovi@gmail.com>
2018-06-06 18:24:34 -0700

    signup form setup

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {Route, Switch } from 'react-router-dom';
 import { AuthRoute, ProtectedRoute } from '../util/route_api_util';
 import LoginFormContainer from './loginform/login_form_container';
+import SignupFormContainer from './signupform/signup_form_container';
 
 const App = () => (
 
 	<div>
 		<Switch>
 			<AuthRoute path="/login" component={LoginFormContainer} />
+			<AuthRoute path="/signup" component={SignupFormContainer} />			
 		</Switch>
 	</div>
 
---
+++ frontend/components/signupform/signup_form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class SignupForm extends React.Component {
+	constructor(props){
+		super(props)
+	}
+
+	render(){
+		return(
+			<div>
+				<h1>This is the SignupForm</h1>
+			</div>
+		)
+	}
+}
+
+export default SignupForm;
